2e63c8346fa1cf24a3fee0eba5b1b4ccf33fba1f
Shaan Vats <shaanvats@Mac-1761.lan>
2025-07-13 00:51:35 -0700

    added tqdm for tracking - perplexity calc - logging

--- train.py
+++ train.py
@@ -11,22 +11,25 @@ from training_utils.setup_dataloaders import (
 )
 
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # Get losses
 train_losses, test_losses = [], []
 
 # Training loop
 epochs = 20
-for epoch in range(epochs):
-    train_loss, train_acc = train_cnn(cnn, train_loader, criterion, optimizer)
-    test_loss, test_acc = test_cnn(cnn, test_loader, criterion)
+for epoch in tqdm(range(epochs), desc="Training/Testing"):
+    train_loss, train_acc, train_perplexity = train_cnn(cnn, train_loader, criterion, optimizer)
+    test_loss, test_acc, test_perplexity = test_cnn(cnn, test_loader, criterion)
 
     # Append to losses lists
     train_losses.append(train_loss)
     test_losses.append(test_loss)
 
     # Display loss
-    print(f"Epoch {epoch + 1}/{epochs} - Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f} Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}")
+    logger.info(f"Epoch: {epoch}")
+    logger.info(f" - Train Loss: {train_loss:.4f} | Train Accuracy: {train_acc:.4f} | Train perplexity: {train_perplexity:.4f}")
+    logger.info(f" - Test Loss: {test_loss:.4f} | Test Accuracy: {test_acc:.4f} | Test perplexity: {test_perplexity:.4f}")
 
 # Plot training vs. test loss
 plt.plot(range(1, epochs + 1), train_losses, label="Train Loss", c="red") # Train loss
